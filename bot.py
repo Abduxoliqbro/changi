a82d389a03f8d8e2fda4b4216c1c147e0c7f5866
Abduxoliqbro <abduxoliqtojiboyev@gmail.com>
2025-11-06 01:46:46 +0500

    Initial aiogram bot

--- bot.py
+++ bot.py
@@ -1,19 +1,31 @@
-import asyncio
-import os
 from aiogram import Bot, Dispatcher, types
-from aiogram.filters import Command
+from aiogram.dispatcher import Dispatcher, filters
+from aiogram.dispatcher.filters import Command
+from aiogram.utils import executor
+import logging
 
-TOKEN = os.getenv("TOKEN")
+API_TOKEN = "8078536942:AAEYmEsxor_i-d2zlTYVst52rL7qTNne4Lo"
 
-bot = Bot(token=TOKEN)
-dp = Dispatcher()
+logging.basicConfig(level=logging.INFO)
 
-@dp.message(Command("start"))
-async def start(message: types.Message):
-    await message.answer("Salom! Bot Koyeb’da ishlayapti 🚀")
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
 
-async def main():
-    await dp.start_polling(bot)
+
+@dp.message_handler(commands=['start'])
+async def start_command(message: types.Message):
+    await message.reply("Salom! Men aiogram 2.x bilan ishlaydigan botman 🤖")
+
+
+@dp.message_handler(Command("help"))
+async def help_command(message: types.Message):
+    await message.answer("Bu bot yordamchi menyusidir.")
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer(message.text)
+
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    executor.start_polling(dp, skip_updates=True)
